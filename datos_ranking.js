e345099adaeeca845b91ef86c5b68610f10f5297
gremiomicasita <gremiomicasita@gmail.com>
2026-08-11 16:59:07 -0700

    Add files via upload

---
+++ datos_ranking.js
@@ -0,0 +1,206 @@
+// Base de datos del Ranking de Mi Casita (30 Miembros)
+const rankingGremio = [
+    {
+        posicion: 1,
+        nombre: "Girlross",
+        foto: "img/perfil1.jpg",
+        dañoAsalto: "2,981,766,500",
+        daño: { basico: "10,356,131", luz: "794,330,024", oscuridad: "0", agua: "0", fuego: "0", tierra: "0" }
+    },
+    {
+        posicion: 2,
+        nombre: "Altria",
+        foto: "img/perfil2.jpg",
+        dañoAsalto: "2,655,326,312",
+        daño: { basico: "0", luz: "0", oscuridad: "0", agua: "0", fuego: "0", tierra: "0" }
+    },
+    {
+        posicion: 3,
+        nombre: "Hank",
+        foto: "img/perfil3.jpg",
+        dañoAsalto: "2,504,331,962",
+        daño: { basico: "512,117,506", luz: "680,063,622", oscuridad: "0", agua: "0", fuego: "761,074,710", tierra: "0" }
+    },
+    {
+        posicion: 4,
+        nombre: "Carol",
+        foto: "img/perfil4.jpg",
+        dañoAsalto: "1,151,161,885",
+        daño: { basico: "481,456,143", luz: "373,805,211", oscuridad: "548,712,002", agua: "446,500,272", fuego: "329,325,935", tierra: "342,983,332" }
+    },
+    {
+        posicion: 5,
+        nombre: "TgREALITI",
+        foto: "img/perfil5.jpg",
+        dañoAsalto: "405,090,230",
+        daño: { basico: "0", luz: "0", oscuridad: "0", agua: "0", fuego: "0", tierra: "0" }
+    },
+    {
+        posicion: 6,
+        nombre: "Death",
+        foto: "img/perfil6.jpg",
+        dañoAsalto: "260,799,400",
+        daño: { basico: "0", luz: "0", oscuridad: "0", agua: "0", fuego: "0", tierra: "0" }
+    },
+    {
+        posicion: 7,
+        nombre: "Joan",
+        foto: "img/perfil7.jpg",
+        dañoAsalto: "241,014,724",
+        daño: { basico: "130,246,801", luz: "0", oscuridad: "13,369,270", agua: "0", fuego: "37,933,553", tierra: "0" }
+    },
+    {
+        posicion: 8,
+        nombre: "GATARI",
+        foto: "img/perfil8.jpg",
+        dañoAsalto: "227,927,223",
+        daño: { basico: "0", luz: "0", oscuridad: "0", agua: "0", fuego: "0", tierra: "0" }
+    },
+    {
+        posicion: 9,
+        nombre: "Newt",
+        foto: "img/perfil9.jpg",
+        dañoAsalto: "142,248,804",
+        daño: { basico: "227,893,929", luz: "154,370,241", oscuridad: "143,144,416", agua: "184,204,365", fuego: "204,354,528", tierra: "181,290,514" }
+    },
+    {
+        posicion: 10,
+        nombre: "Mermes",
+        foto: "img/perfil10.jpg",
+        dañoAsalto: "131,998,237",
+        daño: { basico: "0", luz: "0", oscuridad: "0", agua: "18,233,666", fuego: "0", tierra: "0" }
+    },
+    {
+        posicion: 11,
+        nombre: "Shadow",
+        foto: "img/perfil11.jpg",
+        dañoAsalto: "103,034,838",
+        daño: { basico: "0", luz: "0", oscuridad: "0", agua: "0", fuego: "0", tierra: "0" }
+    },
+    {
+        posicion: 12,
+        nombre: "Pau",
+        foto: "img/perfil12.jpg",
+        dañoAsalto: "78,803,190",
+        daño: { basico: "46,785,088", luz: "0", oscuridad: "0", agua: "0", fuego: "0", tierra: "0" }
+    },
+    {
+        posicion: 13,
+        nombre: "DarkSoul24",
+        foto: "img/perfil13.jpg",
+        dañoAsalto: "49,242,134",
+        daño: { basico: "0", luz: "0", oscuridad: "0", agua: "0", fuego: "0", tierra: "0" }
+    },
+    {
+        posicion: 14,
+        nombre: "Shushú",
+        foto: "img/perfil14.jpg",
+        dañoAsalto: "45,852,962",
+        daño: { basico: "219,602134", luz: "99,696,195", oscuridad: "62,440,360", agua: "35,335,619", fuego: "114,693,004", tierra: "46,857,231" }
+    },
+    {
+        posicion: 15,
+        nombre: "Migles",
+        foto: "img/perfil15.jpg",
+        dañoAsalto: "44,754,406",
+        daño: { basico: "0", luz: "12M", oscuridad: "17M", agua: "11M", fuego: "13M", tierra: "14M" }
+    },
+    {
+        posicion: 16,
+        nombre: "Orquidisea",
+        foto: "img/perfil16.jpg",
+        dañoAsalto: "39,813,923",
+        daño: { basico: "0", luz: "0", oscuridad: "0", agua: "0", fuego: "0", tierra: "0" }
+    },
+    {
+        posicion: 17,
+        nombre: "Sota",
+        foto: "img/perfil17.jpg",
+        dañoAsalto: "37,514,922",
+        daño: { basico: "0", luz: "10M", oscuridad: "15M", agua: "9.5M", fuego: "11M", tierra: "12M" }
+    },
+    {
+        posicion: 18,
+        nombre: "Luke",
+        foto: "img/perfil18.jpg",
+        dañoAsalto: "28,945,186",
+        daño: { basico: "0", luz: "9.5M", oscuridad: "14M", agua: "9M", fuego: "10M", tierra: "11M" }
+    },
+    {
+        posicion: 19,
+        nombre: "Maconha",
+        foto: "img/perfil19.jpg",
+        dañoAsalto: "9,278,707",
+        daño: { basico: "0", luz: "9M", oscuridad: "13M", agua: "8.5M", fuego: "9.5M", tierra: "10M" }
+    },
+    {
+        posicion: 20,
+        nombre: "St4r",
+        foto: "img/perfil20.jpg",
+        dañoAsalto: "7,726,548",
+        daño: { basico: "19,493,781", luz: "24,186,039", oscuridad: "0", agua: "0", fuego: "19,985,736", tierra: "35,157,822" }
+    },
+    {
+        posicion: 21,
+        nombre: "Zhangyuan",
+        foto: "img/perfil21.jpg",
+        dañoAsalto: "1,125,107",
+        daño: { basico: "0", luz: "8M", oscuridad: "11M", agua: "7.5M", fuego: "8.5M", tierra: "9M" }
+    },
+    {
+        posicion: 22,
+        nombre: "Hope",
+        foto: "img/perfil22.jpg",
+        dañoAsalto: "233,572",
+        daño: { basico: "0", luz: "7.5M", oscuridad: "10M", agua: "7M", fuego: "8M", tierra: "8.5M" }
+    },
+    {
+        posicion: 23,
+        nombre: "Kami",
+        foto: "img/perfil23.jpg",
+        dañoAsalto: "8,767",
+        daño: { basico: "0", luz: "0", oscuridad: "0", agua: "0", fuego: "0", tierra: "0" }
+    },
+    {
+        posicion: 24,
+        nombre: "Espacio libre",
+        foto: "img/perfil24.jpg",
+        daño: { basico: "0", luz: "6.5M", oscuridad: "9M", agua: "6M", fuego: "7M", tierra: "7.5M" }
+    },
+    {
+        posicion: 25,
+        nombre: "Espacio libre",
+        foto: "img/perfil25.jpg",
+        daño: { basico: "0", luz: "6M", oscuridad: "8.5M", agua: "5.5M", fuego: "6.5M", tierra: "7M" }
+    },
+    {
+        posicion: 26,
+        nombre: "Espacio libre",
+        foto: "img/perfil26.jpg",
+        daño: { basico: "0", luz: "5.5M", oscuridad: "8M", agua: "5M", fuego: "6M", tierra: "6.5M" }
+    },
+    {
+        posicion: 27,
+        nombre: "Espacio libre",
+        foto: "img/perfil27.jpg",
+        daño: { basico: "0", luz: "5M", oscuridad: "7.5M", agua: "4.5M", fuego: "5.5M", tierra: "6M" }
+    },
+    {
+        posicion: 28,
+        nombre: "Espacio libre",
+        foto: "img/perfil28.jpg",
+        daño: { basico: "0", luz: "4.5M", oscuridad: "7M", agua: "4M", fuego: "5M", tierra: "5.5M" }
+    },
+    {
+        posicion: 29,
+        nombre: "Espacio libre",
+        foto: "img/perfil29.jpg",
+        daño: { basico: "0", luz: "4M", oscuridad: "6.5M", agua: "3.5M", fuego: "4.5M", tierra: "5M" }
+    },
+    {
+        posicion: 30,
+        nombre: "Espacio libre",
+        foto: "img/perfil30.jpg",
+        daño: { basico: "0", luz: "3.5M", oscuridad: "6M", agua: "3M", fuego: "4M", tierra: "4.5M" }
+    }
+];
